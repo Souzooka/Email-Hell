2be75a57fb904d8c28420a5aa0f50a72b390596a
Dakota <bazookaharwell@gmail.com>
2017-03-27 09:41:27 -1000

    return an obj of a tally of individual emails

--- app.js
+++ app.js
@@ -2,7 +2,19 @@ function duplicateEmailFinder() {
 
   // entry function, will process an object of emails and return a tally of e-mails which show up more than once.
   function processData(obj) {
+    let emails = obj.emails;
+    let emailsTally = {};
 
+    for (let i = 0; i < emails.length; i++) {
+      let email = emails[i].email;
+
+      if (!(emailsTally.hasOwnProperty(email))) {
+        emailsTally[email] = 1;
+      } else {
+        emailsTally[email]++;
+      }
+    }
+    return emailsTally;
   }
 
   return {
@@ -10,11 +22,6 @@ function duplicateEmailFinder() {
   };
 
 }
-
-
-let emailChecker = null;
-
-
 
 // Test Data
 
@@ -964,3 +971,7 @@ const DATA = {
 }
 
 
+
+let emailChecker = duplicateEmailFinder();
+let tally = emailChecker.processData(DATA);
+console.log(tally);
